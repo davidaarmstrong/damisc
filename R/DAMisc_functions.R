fd146d2558b0cec1c99ebbbb56fbe469df8fe215
Dave Armstrong <david@MacBook-Pro.local>
2018-07-25 14:37:19 -0400

    updated yj_trans

--- R/DAMisc_functions.R
+++ R/DAMisc_functions.R
@@ -3059,7 +3059,7 @@ optim_yj <- function(pars, form, data, trans.vars, ...){
     ll <- logLik(m)   
     -ll
 }
-    opt.pars <- nlminb(rep(1, length(transvars)), optim_yj, form=form, data=data, trans.vars = trans.vars, lower=0, upper=2)
+    opt.pars <- nlminb(rep(1, length(trans.vars)), optim_yj, form=form, data=data, trans.vars = trans.vars, lower=0, upper=2)
     if(!is.null(round.digits)){
         opt.pars$par <- round(opt.pars$par, round.digits)
     }
